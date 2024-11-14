40c65dae3e58d513c8531f7fe4a9a588d9796368
pinalli <betopinali@gmail.com>
2024-11-14 13:38:28 -0300

    refact: métodos do crud refatorados para utilizar o ResponseEntity e retornar os códigos HTTP corretos

--- api/src/main/java/br/com/pinalli/med/voll/api/controller/DoctorController.java
+++ api/src/main/java/br/com/pinalli/med/voll/api/controller/DoctorController.java
@@ -1,15 +1,14 @@
 package br.com.pinalli.med.voll.api.controller;
 
-import br.com.pinalli.med.voll.api.model.DataListDoctor;
-import br.com.pinalli.med.voll.api.model.DataRegisterDoctor;
-import br.com.pinalli.med.voll.api.model.DataUpdateRegisterDoctor;
-import br.com.pinalli.med.voll.api.model.Doctor;
+import br.com.pinalli.med.voll.api.model.*;
 import br.com.pinalli.med.voll.api.repository.DoctorRepository;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @RestController
@@ -24,27 +23,34 @@ public class DoctorController {
 
     @PostMapping
     @Transactional
-    public void register(@RequestBody @Valid DataRegisterDoctor data){
-        repository.save(new Doctor(data));
+    public  ResponseEntity<DetailsDataDoctor> register(@RequestBody @Valid DataRegisterDoctor data, UriComponentsBuilder uriBuilder){
+        var doctor = new Doctor(data);
+        repository.save(doctor);
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(doctor.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DetailsDataDoctor(doctor));
     }
 
     @GetMapping
-    public Page<DataListDoctor> list(Pageable pagination){
-        return repository.findAllByActiveTrue(pagination).map(DataListDoctor::new);
+    public  ResponseEntity<Page<DataListDoctor>>list(Pageable pagination){
+       var page =  repository.findAllByActiveTrue(pagination).map(DataListDoctor::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void update(@RequestBody @Valid DataUpdateRegisterDoctor data){
+    public  ResponseEntity<DetailsDataDoctor> update(@RequestBody @Valid DataUpdateRegisterDoctor data){
         var doctor = repository.getReferenceById(data.id());
         doctor.updateData(data);
+        return ResponseEntity.ok(new DetailsDataDoctor(doctor));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable Long id){
+    public ResponseEntity<Doctor> delete(@PathVariable Long id){
        // repository.deleteById(id); //exclusao fisica
         var doctor = repository.getReferenceById(id); //exclusao lógica
         doctor.delete();
+
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ api/src/main/java/br/com/pinalli/med/voll/api/model/DetailsDataDoctor.java
@@ -0,0 +1,23 @@
+package br.com.pinalli.med.voll.api.model;
+
+public record DetailsDataDoctor(
+        Long id,
+        String nome,
+        String email,
+        String telefone,
+        String crm,
+        SpecialtyDoctor especialidade,
+        AddressDoctor endereco
+) {
+    public DetailsDataDoctor(Doctor doctor) {
+        this(
+                doctor.getId(),
+                doctor.getNome(),
+                doctor.getEmail(),
+                doctor.getTelefone(),
+                doctor.getCrm(),
+                doctor.getEspecialidade(),
+                doctor.getEndereco()
+        );
+    }
+}
